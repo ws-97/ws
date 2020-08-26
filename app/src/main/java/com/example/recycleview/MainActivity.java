23be638edae1c2d8848140dcc7d33892a816fdfc
ws-97 <15251825502@163.com>
2020-08-26 14:09:20 +0800

    test

--- app/src/main/java/com/example/recycleview/MainActivity.java
+++ app/src/main/java/com/example/recycleview/MainActivity.java
@@ -1,20 +1,31 @@
 package com.example.recycleview;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.listener.OnItemClickListener;
 
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnItemClickListener {
 
     private RecyclerView rvtest;
+    private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context=this;
         initView();
     }
 
@@ -26,8 +37,17 @@ public class MainActivity extends AppCompatActivity {
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         rvtest.setLayoutManager(layoutManager);
         TestAdapter testAdapter=new TestAdapter(R.layout.testitem, Arrays.asList("1","2","3"));//传入布局和数据源
+        testAdapter.setOnItemClickListener(this);
         rvtest.setAdapter(testAdapter);
+
     }
 
-
+    private static final String TAG = "MainActivity";
+    @Override
+    public void onItemClick(@NonNull BaseQuickAdapter<?, ?> adapter, @NonNull View view, int position) {
+        Log.e(TAG, "onItemClick: "+position );
+        TextView textView= view.findViewById(R.id.tv_text1);
+        textView.setVisibility(View.VISIBLE);
+        textView.setText("12");
+    }
 }
